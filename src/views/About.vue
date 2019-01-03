e6c05ad52d18a1e66b7af3c9373bec09341dfa46
Shruan <qiushiyuan1994@qq.com>
2019-01-04 03:41:26 +0800

    引入iview 按需加载

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Button>This is an about page</Button>
+    <Table/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import { Button, Table } from 'iview'
+
+export default {
+  name: 'About',
+  components: {
+    Button,
+    Table
+  }
+}
+</script>
