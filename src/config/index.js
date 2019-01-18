6b4aa346ef9e3cbb9c467e6073fd9ff3abb727d6
Shruan <qiushiyuan1994@qq.com>
2019-01-18 10:51:47 +0800

    增加全局配置

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,9 @@
 export default {
-  baseUrl: {
+  HomePage: 'HomePage',
+  // 使用环境 dev、pro
+  NODE_ENV: process.env.NODE_ENV,
+  // 请求路由
+  baseURL: {
     dev: 'https://www.easy-mock.com/mock/5c2e68211707a03bccd1f12f/iview-admin',
     pro: 'https://production.com'
   }
--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import config from './config'
+
 import './assets/theme.less'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$config = config
 
 new Vue({
   router,
