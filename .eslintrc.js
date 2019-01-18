db9341b9f4038ea7ad18717581207bb6acca4634
Shruan <qiushiyuan1994@qq.com>
2019-01-18 11:01:09 +0800

    eslint adjust

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  rules: {
+    'no-console': 'off',
+    'object-curly-spacing': 'off',
+    'no-tabs': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "vue/no-parsing-error": [2, { "x-invalid-end-tag": false }]
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
