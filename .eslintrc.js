1e39ffb6af6551fb1071070f237c43c4ef5ccf87
Shruan <qiushiyuan1994@qq.com>
2019-01-19 14:58:20 +0800

    登录接口 增加eslint修复

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'object-curly-spacing': 'off',
+    'no-return-assign': 'off',
     'no-tabs': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     "vue/no-parsing-error": [2, { "x-invalid-end-tag": false }]
